d733a78bdbaabb9905d48d5cf09d43fcd554d8ee
Beatriz de Brito <beatrizdb@ciandt.com>
2022-12-26 09:05:00 -0300

    Adiciona componente foto

---
+++ src/app/photo/photo.componet.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core"
+
+@Component({
+    selector: 'app-photo',
+    templateUrl:'photo.component.html'
+
+})
+export class PhotoComponent {
+
+    description='gato';
+}
