95989f24306cf7b5e18eb90b85ed2ae9a04205ff
Beatriz de Brito <beatrizdb@ciandt.com>
2022-12-27 09:40:54 -0300

    Exclui arquivos não utilizados e adiciona arquivos do novo projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-photo',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'alurapic';
+  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'comer', done: true },
+    { description: 'dormir', done: false },
+    { description: 'jogar', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+
 }
